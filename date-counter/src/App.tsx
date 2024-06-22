7ebbe5c10ee5eb72d23dd3dffa5699ea3a847346
soner3 <sonerastan@icloud.com>
2024-06-22 14:05:46 +0200

    Finished Date Counter

---
+++ date-counter/src/App.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+  const [date, setDate] = useState<Date>(new Date());
+  const format: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const daysOfWeek: string[] = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+
+  function handleStepMinus() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleStepPlus() {
+    setStep(step + 1);
+  }
+
+  function handleCountMinus() {
+    setCount(count - step);
+    setDate(new Date(date.setDate(date.getDate() - step)));
+  }
+
+  function handleCountPlus() {
+    setCount(count + step);
+    setDate(new Date(date.setDate(date.getDate() + step)));
+  }
+
+  return (
+    <main className="flex flex-col justify-center text-xl text-center font-medium font-sans">
+      <h1 className="m-2 text-3xl font-bold">Date Counter</h1>
+      <div className="flex justify-center items-center m-2 gap-5">
+        <button
+          className="border-2 border-black w-9 bg-red-500 text-2xl font-bold hover:bg-red-600"
+          onClick={handleStepMinus}
+        >
+          -
+        </button>
+        <h2>Step: {step}</h2>
+
+        <button
+          className="border-2 border-black w-9 bg-green-500 text-2xl font-bold hover:bg-green-600"
+          onClick={handleStepPlus}
+        >
+          +
+        </button>
+      </div>
+      <div className="flex justify-center items-center m-2 gap-5">
+        <button
+          className="border-2 border-black w-9 bg-red-500 text-2xl font-bold hover:bg-red-600"
+          onClick={handleCountMinus}
+        >
+          -
+        </button>
+        <h2>Count: {count}</h2>
+        <button
+          className="border-2 border-black w-9 bg-green-500 text-2xl font-bold hover:bg-green-600"
+          onClick={handleCountPlus}
+        >
+          +
+        </button>
+      </div>
+      <div className="flex justify-center items-center m-2 ">
+        <h2 className="text-2xl">
+          {count === 0
+            ? "Today is"
+            : `${count} day${
+                count === 1 || count === -1 ? "" : "s"
+              } from today is`}{" "}
+          {daysOfWeek[date.getDay()]} {date.toLocaleDateString("en-US", format)}
+        </h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
--- steps/src/App.tsx
+++ steps/src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState<number>(1);
+  const [isOpen, setIsOpen] = useState<boolean | null>(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((currentStep) => currentStep - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      // setStep((currentStep) => currentStep + 1);
+      setStep((currentStep) => currentStep + 1);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              className="text-white bg-[#7950f2]"
+              onClick={handlePrevious}
+              // onMouseEnter={() => {
+              //   alert("TEST");
+              // }}
+            >
+              Previous
+            </button>
+            <button className="text-white bg-[#7950f2]" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
-
-export default App
