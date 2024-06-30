b92dea91fdcacfa351cc2b84beb1ca7df5acd1f2
soner3 <sonerastan@icloud.com>
2024-06-30 18:57:33 +0200

    finished Movie App

--- usepopcorn/src/main.tsx
+++ usepopcorn/src/main.tsx
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
 // import App from "./App";
-// import "./index.css";
+import "./index.css";
+import App from "./App-v1.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Test />
-    <StarRating maxRating={5} defaultRating={3} />
+    <App />
+    {/* <Test />
+    <StarRating maxRating={5} defaultRating={3} /> */}
   </React.StrictMode>
 );
-
-// eslint-disable-next-line react-refresh/only-export-components
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
