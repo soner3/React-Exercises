d00fca77e91fc7a811643462c229e45addeaa293
soner3 <sonerastan@icloud.com>
2024-06-30 21:43:05 +0200

    Finished Currency Converter

---
+++ currency-converter/src/App.tsx
@@ -0,0 +1,15 @@
+// API: https://api.frankfurter.app/latest
+
+import CurrencyConverter from "./CurrencyConverter";
+
+function App() {
+  return (
+    <>
+      <main className="flex flex-col justify-center items-center h-screen">
+        <CurrencyConverter />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ currency-converter/src/CurrencyConverter.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+
+interface CurrencyAPIType {
+  rates: Record<string, number>;
+}
+
+export default function CurrencyConverter() {
+  const [output, setOutput] = useState("Output");
+  const [value, setValue] = useState("");
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [convertCurrency, setConvertCurrency] = useState("USD");
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  function handleChangeValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+
+  function handleChangeBase(e: React.ChangeEvent<HTMLSelectElement>) {
+    setBaseCurrency(e.target.value);
+  }
+
+  function handleChangeConvert(e: React.ChangeEvent<HTMLSelectElement>) {
+    setConvertCurrency(e.target.value);
+  }
+
+  useEffect(() => {
+    async function changeCurrency() {
+      try {
+        if (value === "") {
+          return;
+        }
+        if (baseCurrency === convertCurrency) {
+          setOutput(value);
+          return;
+        }
+
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${value}&base=${baseCurrency}`
+        );
+
+        const data: CurrencyAPIType = await res.json();
+        console.log(data);
+
+        setOutput(String(data.rates[convertCurrency]));
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+      } finally {
+        // setIsLoading(false);
+      }
+    }
+
+    changeCurrency();
+  }, [value, baseCurrency, convertCurrency]);
+
+  return (
+    <div className="border-2 border-black p-4 rounded-xl shadow-xl">
+      <div className="mb-1">
+        <label htmlFor="value" className="text-lg">
+          Value:
+        </label>
+        <input
+          id="value"
+          type="text"
+          className="border-2 border-black rounded-xl p-1 ml-1"
+          value={value}
+          onChange={handleChangeValue}
+        />
+      </div>
+      <div className="mb-1">
+        <label htmlFor="baseCurrency">Base Currency</label>
+        <select
+          className="ml-1"
+          name="baseCurrency"
+          id="baseCurrency"
+          value={baseCurrency}
+          onChange={handleChangeBase}
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      <div className="mb-1">
+        <label htmlFor="convertCurrency">Convert Currency</label>
+        <select
+          className="ml-1"
+          name="baseCurrency"
+          id="convertCurrency"
+          value={convertCurrency}
+          onChange={handleChangeConvert}
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      <p className="text-2xl font-medium">{output}</p>
+    </div>
+  );
+}
