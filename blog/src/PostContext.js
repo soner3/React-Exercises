e2f317e6c46f5ad0994724b92bef90dc30032aaf
soner3 <sonerastan@icloud.com>
2024-07-13 10:04:03 +0200

    Added Project

--- blog/src/PostContext.js
+++ blog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 // 1) Create new Context
@@ -28,19 +28,18 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery: searchQuery,
-        setSearchQuery: setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+
+      searchQuery: searchQuery,
+      setSearchQuery: setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 
 function createRandomPost() {
--- blog/src/Test.js
+++ blog/src/Test.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function SlowComponent() {
+function SlowComponent({ children }) {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
@@ -10,17 +10,25 @@ function SlowComponent() {
           {i}: {word}
         </li>
       ))}
+      {children}
     </ul>
   );
 }
 
 export default function Test() {
   const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
   return (
     <div>
-      <h1>Slow counter?!?</h1>
+      {/* <h1>SlowContainer</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      <SlowComponent /> */}
     </div>
   );
 }
