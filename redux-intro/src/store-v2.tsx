04e04a50f5883847f500639439932634fe41a9fa
soner3 <sonerastan@icloud.com>
2024-07-18 20:46:13 +0200

    Started new Exercise

--- redux-intro/src/store-v2.tsx
+++ redux-intro/src/store-v2.tsx
@@ -23,8 +23,8 @@ import customerReducer from "./features/customers/customerSlice";
 
 const store = configureStore({
   reducer: {
-    account: accountReducer,
     customer: customerReducer,
+    account: accountReducer,
   },
 });
 
