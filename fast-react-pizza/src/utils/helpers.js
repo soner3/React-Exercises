f866b531d259822c388397e2bf5bdb54206213c2
soner3 <sonerastan@icloud.com>
2024-07-20 09:49:34 +0200

    Added Tailwind

--- fast-react-pizza/src/utils/helpers.js
+++ fast-react-pizza/src/utils/helpers.js
@@ -1,11 +1,11 @@
-export function formatCurrency(value: number | bigint) {
+export function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "EUR",
   }).format(value);
 }
 
-export function formatDate(dateStr: number | string | Date) {
+export function formatDate(dateStr) {
   return new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "short",
@@ -14,7 +14,7 @@ export function formatDate(dateStr: number | string | Date) {
   }).format(new Date(dateStr));
 }
 
-export function calcMinutesLeft(dateStr: number | string | Date) {
+export function calcMinutesLeft(dateStr) {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
